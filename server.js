9df99d48afe5196f0332b716eef37e5396ad19d1
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-10 11:06:06 +0100

    server pulls in results and redirects to database requests

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ const createRouter = require('./helpers/create_router.js');
 MongoClient.connect('mongodb://localhost:27017')
   .then((client) => {
     const db = client.db('solar_system');
+
     const planetsCollection = db.collection('planets');
     const quizesCollection = db.collection('quizes');
+    const resultsCollection = db.collection('results');
+
     const planetsRouter = createRouter(planetsCollection);
     const quizesRouter = createRouter(quizesCollection);
+    const resultsRouter = createRouter(resultsCollection);
+
     app.use('/api/planets', planetsRouter);
     app.use('/api/quizes', quizesRouter);
+    app.use('/api/results', resultsRouter);
   })
   .catch(console.err);
 
